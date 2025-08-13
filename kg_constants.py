e45aa22acd7a857581174d17e624596bec5db015
Dennis <dennis@houselewis.com>
2025-08-13 08:50:13 -0700

    Add is_provisional constant for knowledge graph metadata tracking

--- kg_constants.py
+++ kg_constants.py
@@ -5,6 +5,7 @@
 KG_REL_CHAPTER_ADDED = "chapter_added"
 KG_NODE_CREATED_CHAPTER = "created_chapter"
 KG_NODE_CHAPTER_UPDATED = "chapter_updated"
+KG_IS_PROVISIONAL = "is_provisional"
 
 
 # --- Canonical Schema Definition ---
